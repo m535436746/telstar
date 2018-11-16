6f42ded009bfa29d7da3ba5552c40164839ef968
ming <13635853392@163.com>
2018-11-16 11:30:19 +0800

    update launcher.java

--- javaCode/launcher.java
+++ javaCode/launcher.java
@@ -1,6 +1,9 @@
-public static void main(String [] args){
-	String name = "huasheng";
-	int i = 10;
-	system.out.println(name+i);
+public class launcher{
+	public static void main(String [] args){
+		String name = "huasheng";
+		int i = 10;
+		System.out.println(name+i);
 
+	}
 }
+
